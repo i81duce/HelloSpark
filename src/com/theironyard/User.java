33cfba648ea05c6d7903327450fb3d9b3d533593
KevinBozic <bozic.kevin@gmail.com>
2016-02-23 10:08:57 -0500

    Initial import

---
+++ src/com/theironyard/User.java
@@ -0,0 +1,15 @@
+package com.theironyard;//Created by KevinBozic on 2/22/16.
+
+public class User {
+    String name;
+    String password;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+}
