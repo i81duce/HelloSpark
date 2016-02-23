b9ed149c3ea8eef270dca8cf29fb9bcec36c30ad
KevinBozic <bozic.kevin@gmail.com>
2016-02-23 10:36:50 -0500

    Add multi-user support

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -8,7 +9,7 @@ import java.util.HashMap;
 
 public class Main {
 
-    static User user;
+    static HashMap<String, User> users = new HashMap<>();
 
     public static void main(String[] args) {
         Spark.init();
@@ -17,6 +18,11 @@ public class Main {
                 "/",
                 ((request, response) -> {
                     HashMap m = new HashMap();
+
+                    Session session = request.session();
+                    String userName = session.attribute("userName");
+                    User user = users.get(userName);
+
                     if (user == null) {
                         return new ModelAndView(m, "login.html");
                     }
@@ -32,7 +38,25 @@ public class Main {
                 "/login",
                 ((request, response) -> {
                     String name = request.queryParams("loginName");
-                    user = new User(name);
+                    User user = users.get(name);
+                    if (user == null) {
+                        user = new User(name); //this creates new user
+                        users.put(name, user); //this puts it in the hashmap
+                    }
+
+                    Session session = request.session(); // ability to store things
+                    session.attribute("userName", name);
+
+                    response.redirect("/");
+                    return "";
+                })
+        );
+
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate(); //wipes out current session
                     response.redirect("/");
                     return "";
                 })
